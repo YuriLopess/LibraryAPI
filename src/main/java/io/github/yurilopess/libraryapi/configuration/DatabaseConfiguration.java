88a4d4e2d4cfd35a6a3bcbb05f3107fc82af34ab
YuriLopess <costalopesyuri@gmail.com>
2025-03-06 22:00:12 -0300

    feat(database): add HikariCP as connection pool

--- src/main/java/io/github/yurilopess/libraryapi/configuration/DatabaseConfiguration.java
+++ src/main/java/io/github/yurilopess/libraryapi/configuration/DatabaseConfiguration.java
@@ -1,7 +1,14 @@
 package io.github.yurilopess.libraryapi.configuration;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.util.DriverDataSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfiguration {
@@ -13,4 +20,19 @@ public class DatabaseConfiguration {
     String password;
     @Value("${spring.datasource.driver-class-name}")
     String driver;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setPoolName("library-db-pool");
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+
+        return new HikariDataSource(config);
+    }
 }
