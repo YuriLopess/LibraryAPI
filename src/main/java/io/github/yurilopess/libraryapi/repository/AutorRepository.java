cc4a102e997103005da171124c06325340918341
YuriLopess <costalopesyuri@gmail.com>
2025-03-11 21:02:38 -0300

    feat: add repository class

---
+++ src/main/java/io/github/yurilopess/libraryapi/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package io.github.yurilopess.libraryapi.repository;
+
+import io.github.yurilopess.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository <Autor, UUID> {
+}
